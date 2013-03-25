97a09164767c55da553754a022da89ddd9ba5224
aldawoudy <ismail.aldawoudy@gmail.com>
2013-03-25 14:07:55 +0100

    French News

--- FrenchNews/src/com/example/frenchnews/SplashActivity.java
+++ FrenchNews/src/com/example/frenchnews/SplashActivity.java
@@ -1,17 +1,48 @@
 package com.example.frenchnews;
 
+import java.util.ArrayList;
+
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
 
 import com.actionbarsherlock.view.Menu;
 
-import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 
-public class SplashActivity extends SherlockActivity {
 
+
+public class SplashActivity extends SherlockFragmentActivity {
+
+	ViewPager mViewPager;
+	TabsAdapter mTabsAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_splash);
+		
+		mViewPager = new ViewPager(this);
+		mViewPager.setId(R.id.pager);
+		
+		setContentView(mViewPager);
+		
+		ActionBar bar = getSupportActionBar();
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
+		mTabsAdapter = new TabsAdapter(this, mViewPager);
+		
+		mTabsAdapter.addTab(bar.newTab().setIcon(R.drawable.social_chat), FragmentOne.class, null);
+		mTabsAdapter.addTab(bar.newTab().setIcon(R.drawable.social_person), FragmentTwo.class, null);
+		mTabsAdapter.addTab(bar.newTab().setIcon(R.drawable.social_group), FragmentOne.class, null);
+		mTabsAdapter.addTab(bar.newTab().setIcon(R.drawable.location_map), FragmentTwo.class, null);
+		mTabsAdapter.addTab(bar.newTab().setIcon(R.drawable.action_settings), FragmentOne.class, null);
+		
 	}
 
 	
@@ -20,6 +51,102 @@ public class SplashActivity extends SherlockActivity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getSherlock().getMenuInflater().inflate(R.menu.activity_itemlist, menu);
 		return true;
+	}
+	
+	public static class TabsAdapter extends FragmentPagerAdapter implements
+	ActionBar.TabListener, ViewPager.OnPageChangeListener
+	{
+
+		private final Context mContext;
+		private final ActionBar mActionBar;
+		private final ViewPager mViewPager;
+		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+		
+		static final class TabInfo
+		{
+			private final Class<?> clss;
+			private final Bundle args;
+
+			TabInfo(Class<?> _class, Bundle _args)
+			{
+				clss = _class;
+				args = _args;
+			}
+		}
+
+		public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager) {
+			super(activity.getSupportFragmentManager());
+			mContext = activity;
+			mActionBar = activity.getSupportActionBar();
+			mViewPager = pager;
+			mViewPager.setAdapter(this);
+			mViewPager.setOnPageChangeListener(this);
+		}
+		
+		public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args)
+		{
+			TabInfo info = new TabInfo(clss, args);
+			tab.setTag(info);
+			tab.setTabListener(this);
+			mTabs.add(info);
+			mActionBar.addTab(tab);
+			notifyDataSetChanged();
+		}
+
+		@Override
+		public void onPageScrollStateChanged(int arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onPageScrolled(int arg0, float arg1, int arg2) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onPageSelected(int position) {
+			mActionBar.setSelectedNavigationItem(position);
+			
+		}
+
+		@Override
+		public void onTabSelected(Tab tab, FragmentTransaction ft)
+		{
+			Object tag = tab.getTag();
+			for (int i = 0; i < mTabs.size(); i++)
+			{
+				if (mTabs.get(i) == tag)
+				{
+					mViewPager.setCurrentItem(i);
+				}
+			}
+		}
+
+		@Override
+		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void onTabReselected(Tab tab, FragmentTransaction ft) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public Fragment getItem(int position) {
+			TabInfo info = mTabs.get(position);
+			return Fragment.instantiate(mContext, info.clss.getName(), info.args);
+		}
+
+		@Override
+		public int getCount() {
+			return mTabs.size();
+		}
+			
 	}
 	
 
